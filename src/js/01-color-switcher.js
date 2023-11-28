b8bfa82a64ecb2fa1982848d6b8df0a07c1fd146
BartoszRajewicz <szejn3@gmail.com>
2023-11-28 19:13:17 +0100

    feat: added code for start button with onclick action

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const startButton = document.querySelector(".data-start");
-const stopButton = document.querySelector("data-stop");
+const startButton = document.querySelector('.data-start');
+const stopButton = document.querySelector('data-stop');
 let timerId = null;
+
+startButton.addEventListener('click', () => {
+  timerId = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
