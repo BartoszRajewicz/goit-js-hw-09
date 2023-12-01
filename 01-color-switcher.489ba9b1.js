3eace8e251dc467c539fe082c021d695781065dc
BartoszRajewicz <117021647+BartoszRajewicz@users.noreply.github.com>
2023-12-01 22:14:38 +0000

    Deploying to gh-pages from @ BartoszRajewicz/goit-js-hw-09@5807c5a24ee50588076020c1c27915788212a2f4 🚀

--- 01-color-switcher.489ba9b1.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector(".data-start"),e=document.querySelector("data-stop");let o=null;t.addEventListener("click",(()=>{o=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.489ba9b1.js.map
