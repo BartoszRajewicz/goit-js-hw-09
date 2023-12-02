3faa2cbde2be1acf4ed4ad0c52dc100d3bbd60d8
BartoszRajewicz <szejn3@gmail.com>
2023-12-02 22:44:17 +0100

    feat: added addLeadingZero function

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,7 +7,9 @@ const dateTimePicker = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('[data-start]');
 
 startButton.addEventListener('click', () => {
-  startButton.disabled = true;
+    startButton.disabled = true;
+    
+    
 });
 
 const options = {
@@ -50,3 +52,7 @@ function convertMs(ms) {
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
