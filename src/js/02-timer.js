b475130cdc4a89c64bc631293785e225ee21b51a
BartoszRajewicz <szejn3@gmail.com>
2023-12-02 22:16:37 +0100

    added start button listener and flatpickr object

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,3 +3,19 @@ import flatpickr from 'flatpickr';
 // Dodatkowy import stylów
 import 'flatpickr/dist/flatpickr.min.css';
 
+const dateTimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+});
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
