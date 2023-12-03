d1183c5e4e9ed34192c23fdd6f4dfdaf90766fa1
BartoszRajewicz <szejn3@gmail.com>
2023-12-03 15:24:21 +0100

    feat: added const to get input values

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,20 +3,24 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  function createPromise(position, delay) {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      return Promise.resolve({ position, delay });
-    } else {
-      return Promise.reject({ position, delay });
-    }
-  }
+  const firstDelayInput = form.querySelector('input[name = "delay"]');
+  const delayStepInput = form.querySelector('input[name = "step"]');
+  const amountInput = form.querySelector('input[name = "amount"]');
+
+  createPromise(i, delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 });
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    return Promise.resolve({ position, delay });
+  } else {
+    return Promise.reject({ position, delay });
+  }
+}
